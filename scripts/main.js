e915bfe083e8f77ebafbd55b18193dbd68a330ea
Bowie <marketing@letscode.com.br>
2024-09-11 20:31:55 -0300

    concessionaria

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+
+
+// myImage.onclick = () => {
+//     const myScr = myImage.getAttribute("src");
+//     if(mySrc === "images/imagem-para-teste-de-site") {
+//         myImage.setAttribute("src", "images/firefox2.png");} else {
+//             myImage.setAttribute("src",  "images/imagem-para-teste-de-site");
+//         }
+// };
+// const myImage = document.querySelector("img");
+
+  
